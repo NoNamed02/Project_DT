28c222c631dd1f03dd759ca927b39e8bdd365090
NoNamed02 <128911424+NoNamed02@users.noreply.github.com>
2025-09-23 16:43:34 +0900

    feat : card deck handling, draw card to drawdeck

--- Assets/01.Scripts/Fight/Player.cs
+++ Assets/01.Scripts/Fight/Player.cs
@@ -2,22 +2,48 @@ using UnityEngine;
 
 public class Player : Character
 {
+    [Header("Decks")]
+    [SerializeField]
+    private Deck _drawDeck;
+    [SerializeField]
+    private Deck _handDeck;
+    [SerializeField]
+    private Deck _graveyardDeck;
+    public event System.Action<int> OnDrawCard;
 
     void Start()
     {
-        // if (TurnManager.Instance != null)
-        //     TurnManager.Instance.OnTurnChanged += TurnChanged;
+        if (TurnManager.Instance != null)
+            TurnManager.Instance.OnTurnChanged += TurnChanged;
     }
 
-    // private void TurnChanged(TurnManager.TurnOwner owner)
-    // {
-    //     if (owner == TurnManager.TurnOwner.Player)
-    //     {
-    //         Debug.Log("Player's Turn Start");
-    //     }
-    //     else
-    //     {
-    //         Debug.Log("Player's Turn End");
-    //     }
-    // }
+    private void TurnChanged(TurnManager.TurnOwner owner)
+    {
+        if (owner == TurnManager.TurnOwner.Player)
+        {
+            Debug.Log("Player's Turn Start");
+            DrawCard(1);
+        }
+        else
+        {
+            Debug.Log("Player's Turn End");
+        }
+    }
+
+    private void DrawCard(int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            if (!_drawDeck.IsDeckEmpty())
+            {
+                int drawCardId = _drawDeck.DrawCard();
+                _handDeck.SetCardToTop(drawCardId);
+                OnDrawCard.Invoke(drawCardId);
+            }
+            else
+            {
+                Debug.Log("draw deck is empty");
+            }
+        }
+    }
 }
