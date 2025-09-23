cd03d1c91bac86c42ec14abcbceb89bbeed3d781
NoNamed02 <128911424+NoNamed02@users.noreply.github.com>
2025-09-23 16:40:57 +0900

    feat : card default setting = set id to card object

--- Assets/01.Scripts/Card/Card.cs
+++ Assets/01.Scripts/Card/Card.cs
@@ -1,16 +1,19 @@
+using TMPro;
 using UnityEngine;
 
 public class Card : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [Header("Card ID")]
+    [SerializeField]
+    private int _cardID = -1; // 초기값, -1인 상태로 사용되면 예외처리 핸들링
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// 카드의 id를 세팅 = card manager에서 call해서 사용
+    /// </summary>
+    /// <param name="id"></param>
+    public void SetCardID(int id)
     {
-        
+        _cardID = id;
+        transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = _cardID.ToString();
     }
 }
