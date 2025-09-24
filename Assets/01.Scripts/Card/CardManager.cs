b2551ef1ab75b9d916440e64b8dd6a663cb2e0d7
NoNamed02 <128911424+NoNamed02@users.noreply.github.com>
2025-09-25 02:55:24 +0900

    feat : used card func +

--- Assets/01.Scripts/Card/CardManager.cs
+++ Assets/01.Scripts/Card/CardManager.cs
@@ -18,14 +18,36 @@ public class CardManager : MonoBehaviour
     [SerializeField]
     private Player _player;
 
+
+    [Header("select card")]
+    [SerializeField]
+    private Card _selectCard;
+
+    [SerializeField]
+    private Canvas _canvas;          // 최상위 Canvas
+
     void Start()
     {
         _player.OnDrawCard += InstanceCard;
     }
 
+    /// <summary>
+    /// 카드 드로우시 생성
+    /// </summary>
+    /// <param name="cardID"></param>
     public void InstanceCard(int cardID)
     {
         GameObject card = Instantiate(_card, _handArea);
-        card.GetComponent<Card>().SetCardID(cardID);
+        card.GetComponent<Card>().Init(_canvas, cardID);
+    }
+
+    /// <summary>
+    /// 카드 효과 호출 함수
+    /// </summary>
+    /// <param name="target"></param>
+    /// <param name="cardID"></param>
+    public void UsedCard(Character target, int cardID)
+    {
+        Debug.Log($"target:{target} / card : {cardID}");
     }
 }
