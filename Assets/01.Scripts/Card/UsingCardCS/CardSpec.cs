d8976176d55400c5a7316e1fdd8cb40ee5d94eb0
Yukari\Yuka <knite7989@gmail.com>
2025-10-09 15:06:01 +0900

    fix : card spec list change - temp

--- Assets/01.Scripts/Card/UsingCardCS/CardSpec.cs
+++ Assets/01.Scripts/Card/UsingCardCS/CardSpec.cs
@@ -3,10 +3,13 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Cards/CardDefinition")]
 public class CardSpec : ScriptableObject
 {
+    // 나중에 enum으로 정리하는 것을 고려해봐야 할 것 같다
     public int id;
     public string cardName;
     public int cost;
-    // public TargetingMode targeting;
-    public Sprite artwork;
-    public CardEffect[] effects;
+    public string type;
+    public string instruction;
+    public string[] targeting;
+    public string rarity;
+    public string discardPolicy;
 }
