3f94ce54345b9f8f24d3ccf12a35b2dd6fb462a7
NoNamed02 <128911424+NoNamed02@users.noreply.github.com>
2025-10-10 15:41:23 +0900

    feat : card effect DB mechanism

--- Assets/01.Scripts/Card/UsingCardCS/CardDatabase.cs
+++ Assets/01.Scripts/Card/UsingCardCS/CardDatabase.cs
@@ -1,20 +1,59 @@
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class CardDatabase : MonoBehaviour
+public class CardDatabase : MonoSingleton<CardDatabase>
 {
-    [SerializeField] private CardSpec[] cardSpecs;
+    [SerializeField]
+    private CardSpec[] cardSpecs;
 
+    [SerializeField]
+    private CardEffect[] cardDamageEffects;
     private Dictionary<int, CardSpec> _map;
 
-    private void Awake()
+    protected override void Awake()
     {
+        base.Awake();
+
         _map = new Dictionary<int, CardSpec>();
+        // foreach (var spec in cardSpecs)
+        // {
+        //     _map[spec.id] = spec;
+        // }
         foreach (var spec in cardSpecs)
         {
+            if (spec == null)
+            {
+                Debug.LogWarning("CardSpec에 null이 있음");
+                continue;
+            }
             _map[spec.id] = spec;
         }
     }
+    // void Start()
+    // {
+    //     _map = new Dictionary<int, CardSpec>();
+    //     foreach (var spec in cardSpecs)
+    //     {
+    //         _map[spec.id] = spec;
+    //     }
+    // }
 
     public CardSpec Get(int id) => _map.TryGetValue(id, out var spec) ? spec : null;
+
+    /// <summary>
+    /// 카드 생성시 effect list를 카드에 넣기 위한 함수
+    /// </summary>
+    /// <param name="effectName"></param>
+    /// <param name="amount"></param>
+    /// <param name="holdingTime"></param>
+    /// <returns></returns>
+    public CardEffect GetCardEffect(string effectName, int amount, int holdingTime)
+    {
+        if (effectName == "Damage")
+        {
+            return cardDamageEffects[amount];
+        }
+        return null;
+    }
 }
