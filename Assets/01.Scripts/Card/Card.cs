7d4f074f212a673e91f8fe2fab0c815adf6e4421
NoNamed02 <128911424+NoNamed02@users.noreply.github.com>
2025-10-10 15:40:05 +0900

    feat : GUI unit+, card effect == using func+

--- Assets/01.Scripts/Card/Card.cs
+++ Assets/01.Scripts/Card/Card.cs
@@ -1,6 +1,9 @@
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
+using TMPro.EditorUtilities;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.EventSystems;
 
 public class Card : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler
@@ -11,11 +14,36 @@ public class Card : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHand
 
     private Canvas _canvas;
 
+    [SerializeField]
+    private CardSpec _cardSpec;
+    public CardSpec CardSpec { get => _cardSpec; }
+    [SerializeField]
+    private List<CardEffect> _cardEffects = new List<CardEffect>();
+    public List<CardEffect> cardEffects { get => _cardEffects; }
+    // public List<CardEffect> cardEffects { get; }
+
     // if using card, than call this delegate
     public event System.Action<Card, Character> OnUsingCard;
 
+    [SerializeField]
+    private TextMeshProUGUI _costText;
+    [SerializeField]
+    private TextMeshProUGUI _instructionText;
+    [SerializeField]
+    private TextMeshProUGUI _nameText;
+    [SerializeField]
+    private TextMeshProUGUI _idText;
+
+    void Awake()
+    {
+        _costText = transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+        _instructionText = transform.GetChild(1).GetComponent<TextMeshProUGUI>();
+        _nameText = transform.GetChild(2).GetComponent<TextMeshProUGUI>();
+        _idText = transform.GetChild(3).GetComponent<TextMeshProUGUI>();
+    }
+
     /// <summary>
-    /// 카드의 여러 값을 생싱시 세팅
+    /// 카드의 여러 값을 생성시 세팅
     /// </summary>
     /// <param name="id"></param>
     public void Init(Canvas canvas, int id)
@@ -23,6 +51,31 @@ public class Card : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHand
         _canvas = canvas;
         _cardID = id;
         transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = _cardID.ToString();
+
+        _cardSpec = CardDatabase.Instance.Get(_cardID);
+        if (_cardSpec == null)
+        {
+            Debug.LogError($"CardSpec을 찾을 수 없습니다: {_cardID}");
+        }
+        _costText.text = _cardSpec.cost.ToString();
+        _instructionText.text = _cardSpec.instruction.ToString();
+        _nameText.text = _cardSpec.cardName.ToString();
+        _idText.text = _cardSpec.id.ToString();
+
+        for (int i = 0; i < _cardSpec.effect.Length; i++)
+        {
+            Debug.Log(i + "번째 effect add");
+            _cardEffects.Add(CardDatabase.Instance.GetCardEffect(
+                _cardSpec.effect[i], _cardSpec.effectAmount[i], _cardSpec.effectHoldingTime[i]));
+        }
+        // foreach (var effect in _cardEffects)
+        // {
+        //     Debug.Log($"Card에 등록된 Effect: {effect.name}");
+        // }
+        if (_cardEffects[0] == null)
+        {
+            Debug.Log("뭔가 잘못되었다");
+        }
     }
 
     // 드래그 시작 처리
