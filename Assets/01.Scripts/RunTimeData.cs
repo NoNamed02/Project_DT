6722cd9ac6fe5246978a8a7002439a8dc52be0b4
NoNamed02 <128911424+NoNamed02@users.noreply.github.com>
2025-12-01 02:14:17 +0900

    feat : NodeMap Data, Node System

--- Assets/01.Scripts/RunTimeData.cs
+++ Assets/01.Scripts/RunTimeData.cs
@@ -5,4 +5,7 @@ public class RunTimeData : MonoSingleton<RunTimeData>
 {
     public List<int> DeckList = new List<int>();
     public int HP = 100;
+
+    public int LayerIndex = 0;
+    public int CurrentNodeID = 0;
 }
